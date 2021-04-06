e3ab734fcc9f59259dd401d0aa0a7f3803cf0999
kruboy <starkkruboy@gmail.com>
2021-04-06 15:15:28 +0300

    add ticks

--- src/components/Message/index.jsx
+++ src/components/Message/index.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import formatDistanceToNow  from "date-fns/formatDistanceToNow";
+import readTick from 'assets/img/double-tick-indicator.svg';
+import sentTick from 'assets/img/single-tick-indicator.svg';
+
 
 import './Message.scss';
+import classNames from 'classnames';
 
-const Message = ({ avatar, user, text, date }) => {
+const Message = ({ avatar, user, text, date, isMe, isRead }) => {
   return (
-    <div className="message">
+    <div className={classNames("message", {"message-isme": isMe})}>
       <div className="message__avatar">
         <img src={avatar} alt={`Avatar ${user.fullname}`}></img>
       </div>
@@ -16,6 +20,8 @@ const Message = ({ avatar, user, text, date }) => {
         </div>
         <span className="message__date">{formatDistanceToNow(date, { addSuffix: true })}</span>
       </div>
+      {isMe && isRead ? <img className="message__checkedTick" src={readTick} alt=""/> : <img className="message__checkedTick" src={sentTick} alt=""/>}
+
     </div>
   );
 };
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -7,8 +7,14 @@ const Home = () => {
       <Message
         avatar="https://99px.ru/sstorage/1/2010/04/image_10704101336437444814.jpg"
         text="Hello! What's up?"
-        
         date={Date.parse("Tue Apr 06 2021 13:21:31 GMT+0300 (Восточная Европа, летнее время)")}
+      />
+       <Message
+        avatar="https://muratselek.com.tr/wp-content/uploads/2019/01/yorum-icon-avatar.png"
+        text="WOW DUDE! it's you!"
+        date={Date.parse("Tue Apr 06 2021 13:31:31 GMT+0300 (Восточная Европа, летнее время)")}
+        isMe={true}
+        isRead={true}
       />
     </div>
   );
