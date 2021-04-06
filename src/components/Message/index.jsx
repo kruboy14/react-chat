642d493b2fc53d14cc1204bd9a3e982e700a9db7
kruboy <starkkruboy@gmail.com>
2021-04-06 15:50:48 +0300

    added attachments

--- src/components/Message/index.jsx
+++ src/components/Message/index.jsx
@@ -1,27 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import formatDistanceToNow  from "date-fns/formatDistanceToNow";
+import formatDistanceToNow from 'date-fns/formatDistanceToNow';
 import readTick from 'assets/img/double-tick-indicator.svg';
 import sentTick from 'assets/img/single-tick-indicator.svg';
-
 
 import './Message.scss';
 import classNames from 'classnames';
 
-const Message = ({ avatar, user, text, date, isMe, isRead }) => {
+const Message = ({ avatar, user, text, date, isMe, isRead, attachments }) => {
   return (
-    <div className={classNames("message", {"message-isme": isMe})}>
+    <div className={classNames('message', { 'message-isme': isMe })}>
       <div className="message__avatar">
         <img src={avatar} alt={`Avatar ${user.fullname}`}></img>
       </div>
       <div className="message__content">
-        <div className="message__bubble">
-          <p className="message__text">{text}</p>
+        <div className="message__item">
+          <div className="message__bubble">
+            <p className="message__text">{text}</p>
+          </div>
+          <div className="message__attachments">
+            {attachments &&
+              attachments.map((item) => (
+                <div className="message__attachments__item">
+                  <img src={item.url} alt={item.filename} />
+                </div>
+              ))}
+          </div>
         </div>
-        <span className="message__date">{formatDistanceToNow(date, { addSuffix: true })}</span>
+        <span className="message__date">
+          {formatDistanceToNow(date, { addSuffix: true })}
+        </span>
       </div>
-      {isMe && isRead ? <img className="message__checkedTick" src={readTick} alt=""/> : <img className="message__checkedTick" src={sentTick} alt=""/>}
-
+      {isMe && isRead ? (
+        <img className="message__checkedTick" src={readTick} alt="" />
+      ) : (
+        <img className="message__checkedTick" src={sentTick} alt="" />
+      )}
     </div>
   );
 };
@@ -35,6 +49,7 @@ Message.propTypes = {
   user: PropTypes.object,
   text: PropTypes.string,
   date: PropTypes.number,
+  attachments: PropTypes.array,
 };
 
 export default Message;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -7,14 +7,28 @@ const Home = () => {
       <Message
         avatar="https://99px.ru/sstorage/1/2010/04/image_10704101336437444814.jpg"
         text="Hello! What's up?"
-        date={Date.parse("Tue Apr 06 2021 13:21:31 GMT+0300 (Восточная Европа, летнее время)")}
+        date={Date.parse(
+          'Tue Apr 06 2021 13:21:31 GMT+0300 (Восточная Европа, летнее время)',
+        )}
+        attachments={[{
+          filename: 'first.jpg',
+          url: 'https://source.unsplash.com/100x100/?random=1nature',
+        },
+        {
+          filename: 'second.jpg',
+          url: 'https://source.unsplash.com/100x100/?random=2nature',
+        }
+        ]}
       />
-       <Message
+      <Message
         avatar="https://muratselek.com.tr/wp-content/uploads/2019/01/yorum-icon-avatar.png"
         text="WOW DUDE! it's you!"
-        date={Date.parse("Tue Apr 06 2021 13:31:31 GMT+0300 (Восточная Европа, летнее время)")}
+        date={Date.parse(
+          'Tue Apr 06 2021 13:31:31 GMT+0300 (Восточная Европа, летнее время)',
+        )}
         isMe={true}
         isRead={true}
+      
       />
     </div>
   );
